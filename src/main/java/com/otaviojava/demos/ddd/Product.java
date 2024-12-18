c8cece14d361b3dbd0951371a135d5632f66f064
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-18 07:43:26 +0000

    chore: update product style and remove package
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/Product.java
+++ src/main/java/com/otaviojava/demos/ddd/Product.java
@@ -1,19 +1,16 @@
 package com.otaviojava.demos.ddd;
 
-import jakarta.json.bind.annotation.JsonbVisibility;
 import jakarta.nosql.Column;
 import jakarta.nosql.Convert;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
 import org.eclipse.jnosql.databases.mongodb.mapping.ObjectIdConverter;
-import org.soujava.samples.mongodb.products.infra.FieldVisibilityStrategy;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@JsonbVisibility(FieldVisibilityStrategy.class)
 public class Product {
 
     @Id
