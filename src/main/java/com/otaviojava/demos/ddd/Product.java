337a7bac607aa87214e61423c44332c69a462b68
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-18 09:29:43 +0000

    chore: update product with a new constructor
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/Product.java
+++ src/main/java/com/otaviojava/demos/ddd/Product.java
@@ -27,6 +27,36 @@ public class Product {
     @Column
     private Set<Category> categories;
 
+    Product(UUID id, String name, Manufacturer manufacturer,
+            List<String> tags,
+            Set<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.tags = tags;
+        this.categories = categories;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) {
