1f678ff5bffd596638b7a7d1a032ce002ed9b970
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-19 16:23:37 +0000

    feat: create star project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/App.java
+++ src/main/java/com/otaviojava/demos/ddd/App.java
@@ -16,8 +16,11 @@ package com.otaviojava.demos.ddd;
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 import net.datafaker.Faker;
+import net.datafaker.providers.base.Commerce;
 
+import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 
 public class App {
@@ -29,7 +32,14 @@ public class App {
 
             Manufacturer manufacturer = new Manufacturer(faker.company().name(), faker.address().fullAddress(), faker.phoneNumber().cellPhone());
             Set<Category> categories = Set.of(new Category("category1", "description1"), new Category("category2", "description2"));
-
+            Commerce commerce = faker.commerce();
+            Product product = new ProductBuilder()
+                    .id(UUID.randomUUID())
+                    .name(commerce.productName())
+                    .manufacturer(manufacturer)
+                    .tags(List.of("tag1", "tag2"))
+                    .categories(categories)
+                    .build();
         }
     }
 
