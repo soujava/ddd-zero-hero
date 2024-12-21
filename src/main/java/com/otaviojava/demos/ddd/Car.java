9ef4ab17fded3745f681825310971fcfb008bde5
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-21 07:04:49 +0000

    feat: create equals and hascode to car
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/Car.java
+++ src/main/java/com/otaviojava/demos/ddd/Car.java
@@ -1,6 +1,7 @@
 package com.otaviojava.demos.ddd;
 
 import java.time.Year;
+import java.util.Objects;
 
 public class Car {
 
@@ -20,5 +21,50 @@ public class Car {
         this.manufacturer = manufacturer;
         this.year = year;
         this.color = color;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Car car = (Car) object;
+        return Objects.equals(vin, car.vin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(vin);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "vin='" + vin + '\'' +
+                ", transmission='" + transmission + '\'' +
+                ", manufacturer=" + manufacturer +
+                ", year=" + year +
+                ", color=" + color +
+                '}';
     }
 }
