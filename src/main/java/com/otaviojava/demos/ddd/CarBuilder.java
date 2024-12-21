34a5008b3054c1bfee2790d3a33ad3feea850590
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-21 07:03:15 +0000

    define a builder to car
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/CarBuilder.java
+++ src/main/java/com/otaviojava/demos/ddd/CarBuilder.java
@@ -1,2 +1,40 @@
+package com.otaviojava.demos.ddd;
+
+import java.time.Year;
+
 public class CarBuilder {
+    private String vin;
+    private String transmission;
+    private Manufacturer manufacturer;
+    private Year year;
+    private Color color;
+
+    public CarBuilder vin(String vin) {
+        this.vin = vin;
+        return this;
+    }
+
+    public CarBuilder transmission(String transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public CarBuilder manufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+        return this;
+    }
+
+    public CarBuilder year(Year year) {
+        this.year = year;
+        return this;
+    }
+
+    public CarBuilder color(Color color) {
+        this.color = color;
+        return this;
+    }
+
+    public Car build() {
+        return new Car(vin, transmission, manufacturer, year, color);
+    }
 }
