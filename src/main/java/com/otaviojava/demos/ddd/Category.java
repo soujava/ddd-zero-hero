8ce049c389fbbabb79c6b1b2306a93ed1fbe3a4b
Otavio Santana <otaviopolianasantana@gmail.com>
2024-12-21 06:54:01 +0000

    chore: remove pproduct sample and move it Car
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/main/java/com/otaviojava/demos/ddd/Category.java
+++
@@ -1,8 +0,0 @@
-package com.otaviojava.demos.ddd;
-
-import jakarta.nosql.Column;
-import jakarta.nosql.Embeddable;
-
-@Embeddable(Embeddable.EmbeddableType.GROUPING)
-public record Category(@Column String name, @Column String description) {
-}
